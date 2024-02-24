402468e7f8e5b087af737a35d9bcf0df784be42b
fantasticdj <jung112629@gapps.uwcsea.edu.sg>
2024-02-24 09:42:33 +0800

    ad

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    return render_stylesheet("index.css")
 
 if __name__ == "__main__":
     app.run(debug=True)
