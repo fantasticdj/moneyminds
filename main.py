d93a3402e7ca80b7a0ecea450cb26ae250f95d24
Jealous7 <153968747+Jealous7@users.noreply.github.com>
2024-02-24 12:49:07 +0800

    files

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.secret_key = "404 error"
+
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
